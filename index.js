9976a8c46af9a8f4a86566f31e8e7351902c9d76
Fouad Bitar <fouadobitar@gmail.com>
2020-07-01 20:35:57 -0400

    client added

--- index.js
+++ index.js
@@ -40,6 +40,17 @@ connection.once('open', () => {
     console.log("MongoDB database connection established successfully");
 });
 
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+
+    const path = require('path');
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
+
+
 //start app on port
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
